fb5ebab621bc4f443220ac61c020b1cb59c497c5
Henrique Ramos <henrique.ramos@avenuecode.com>
2019-11-28 02:23:41 -0300

    Remove consumers

--- Program.cs
+++ Program.cs
@@ -32,15 +32,6 @@ namespace MessagingApi
                 MessageQueue ftlpQueue = new MessageQueue("subject_logs", "ftlp.*", channel);
                 MessageQueue damdQueue = new MessageQueue("subject_logs", "damd.*", channel);
                 MessageQueue labDamdQueue = new MessageQueue("subject_logs", "labdamd.*", channel);
-                channel.BasicConsume(queue: ftlpQueue.QueueName,
-                                        autoAck: true,
-                                        consumer: consumer.Consumer);
-                channel.BasicConsume(queue: damdQueue.QueueName,
-                                        autoAck: true,
-                                        consumer: consumer.Consumer);
-                channel.BasicConsume(queue: labDamdQueue.QueueName,
-                                        autoAck: true,
-                                        consumer: consumer.Consumer);
             }
             CreateWebHostBuilder(args).Build().Run();
         }
